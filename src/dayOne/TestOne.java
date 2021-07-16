caa8fb5187ab1013089a98faaac93271db3eb18b
unknown <eva073192@yahoo.com>
2021-07-15 23:08:07 -0400

    modifying test one class

--- src/dayOne/TestOne.java
+++ src/dayOne/TestOne.java
@@ -7,19 +7,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TestOne {
+public class TestOne { {
 	
 	//navigate to the URL
 	String url = "https://vax4nyc.nyc.gov/patient/s/";
 	
 	WebDriver driver = new FirefoxDriver();
 	
-
+	System.out.println("Welcome");
+    System.out.println("To");
+    System.out.println("GeeksforGeeks");
 	
 	
 	
-	
-	}
+} }
 
 
 
